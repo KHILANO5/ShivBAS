dd05015a4a1c83ca2a4b60c14c5c0084b9c30d28
Nishit3116 <nishitsavaliya7@gmail.com>
2026-01-31 18:42:04 +0530

    for version3

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -176,25 +176,25 @@ const Login = () => {
                             <div className="absolute inset-0 flex items-center">
                                 <div className="w-full border-t border-gray-300"></div>
                             </div>
-                            <div className="relative flex justify-center text-sm">
+                            {/* <div className="relative flex justify-center text-sm">
                                 <span className="px-2 bg-white text-gray-500">Test Credentials</span>
-                            </div>
+                            </div> */}
                         </div>
 
-                        <div className="mt-4 space-y-2 text-xs text-gray-600 bg-gray-50 p-3 rounded-lg">
+                        {/* <div className="mt-4 space-y-2 text-xs text-gray-600 bg-gray-50 p-3 rounded-lg">
                             <p><strong>Admin:</strong> admin_user / Test@123</p>
                             <p><strong>Portal:</strong> john_portal / Test@123</p>
-                        </div>
+                        </div> */}
 
                         {/* Sign Up Link */}
                         <div className="mt-6">
                             <div className="relative">
                                 <div className="absolute inset-0 flex items-center">
-                                    <div className="w-full border-t border-gray-300"></div>
+                                    {/* <div className="w-full border-t border-gray-300"></div> */}
                                 </div>
-                                <div className="relative flex justify-center text-sm">
+                                {/* <div className="relative flex justify-center text-sm">
                                     <span className="px-2 bg-white text-gray-500">New to ShivBAS?</span>
-                                </div>
+                                </div> */}
                             </div>
 
                             <div className="mt-4 text-center">
